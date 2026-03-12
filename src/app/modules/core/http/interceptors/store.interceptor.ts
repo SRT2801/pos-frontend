450beb1a6855c24a1772ba215ecaf6e578a4f523
SRT DEV <sahirruiz4@gmail.com>
2026-03-11 21:00:09 -0500

    feat: add store interceptor to manage store ID in HTTP requests

---
+++ src/app/modules/core/http/interceptors/store.interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { API_BASE_URL } from '../../../../app.config';
+import { isPlatformBrowser } from '@angular/common';
+
+export const storeIterceptor: HttpInterceptorFn = (req, next) => {
+  const apiBase = inject(API_BASE_URL);
+  const platformId = inject(PLATFORM_ID);
+
+  if (!req.url.startsWith(apiBase)) {
+    return next(req);
+  }
+
+  const storeId = isPlatformBrowser(platformId) ? localStorage.getItem('storeId') : null;
+
+  if (!storeId) {
+    return next(req);
+  }
+
+  const requWithStoreId = req.clone({
+    setHeaders: {
+      'X-Store-ID': storeId,
+    },
+  });
+  return next(requWithStoreId);
+};
