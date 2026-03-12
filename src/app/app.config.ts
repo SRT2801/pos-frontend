a05bdbbdc507b177e8fec3064509550ebabc2565
SRT DEV <sahirruiz4@gmail.com>
2026-03-11 20:55:05 -0500

    refactor: Reorganize imports and formatting in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,21 @@
 import { ApplicationConfig, InjectionToken, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { environment } from '../environments/environment.development';
-import { authInterceptor, errorInterceptor } from './modules/core/http/interceptors/auth.interceptor';
+import { authInterceptor } from './modules/core/http/interceptors/auth.interceptor';
+import { errorInterceptor } from './modules/core/http/interceptors/error.interceptor';
 
 export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor, errorInterceptor])),
-    { provide: API_BASE_URL, useValue: environment.apiUrl }
-
-  ]
+    { provide: API_BASE_URL, useValue: environment.apiUrl },
+  ],
 };
