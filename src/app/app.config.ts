6cdd1187b326291900e1df5e57dca75404b6edde
SRT DEV <sahirruiz4@gmail.com>
2026-03-12 15:51:27 -0500

    refactor: Improve import organization and update HTTP client configuration in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig, InjectionToken, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 import { authInterceptor } from './modules/core/http/interceptors/auth.interceptor';
 import { errorInterceptor } from './modules/core/http/interceptors/error.interceptor';
+import { storeIterceptor } from './modules/core/http/interceptors/store.interceptor';
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL');
 
@@ -15,7 +23,18 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor, errorInterceptor])),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([storeIterceptor, authInterceptor, errorInterceptor]),
+    ),
     { provide: API_BASE_URL, useValue: environment.apiUrl },
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false,
+        },
+      },
+    }),
   ],
 };
