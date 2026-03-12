ddfd0e4eca781f8163f3ef412c9d653c32397e3a
SRT DEV <sahirruiz4@gmail.com>
2026-03-12 15:52:01 -0500

    feat: Define application routes for login, registration, and home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/pages/login/login';
+import { RegisterComponent } from './modules/pages/register-component/register-component';
+import { Layout } from './modules/shared/layout/layout';
+import { Home } from './modules/pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+
+  {
+    path: '',
+    component: Layout,
+    children: [{ path: 'home', component: Home }],
+  },
+];
