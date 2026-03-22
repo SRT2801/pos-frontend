c6a817a794b89cb399a3e60b182366cab2cbd80b
SRT DEV <sahirruiz4@gmail.com>
2026-03-22 17:37:28 -0500

    feat: Integrate ToastModule and enhance Layout component with router dependency and authentication route check

--- src/app/modules/shared/layout/layout.ts
+++ src/app/modules/shared/layout/layout.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Sidebar } from '../components/ui/sidebar/sidebar';
 import { Header } from '../components/ui/header/header';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { SidebarStateService } from '../services/sidebarState.service';
+import { ToastModule } from 'primeng/toast';
+
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterModule, FormsModule, Sidebar, Header],
+  imports: [RouterModule, FormsModule, Sidebar, Header, ToastModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class Layout {
-  constructor(public sidebarState: SidebarStateService) {}
+  constructor(
+    public sidebarState: SidebarStateService,
+    public router: Router,
+  ) {}
+
+  isAuthRoute(): boolean {
+    return this.router.url.includes('/login') || this.router.url.includes('/register');
+  }
 }
