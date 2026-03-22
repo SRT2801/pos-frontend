32a924d9d6403a209e82ec1bef4a87fe6d8c71eb
SRT DEV <sahirruiz4@gmail.com>
2026-03-22 17:35:53 -0500

    feat: Redesign login page with enhanced UI components and form functionality

--- src/app/modules/pages/login/login.ts
+++ src/app/modules/pages/login/login.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';;
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Footer } from '../../shared/components/ui/footer/footer';
+
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule,FormsModule,Footer],
+  imports: [CommonModule,FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
