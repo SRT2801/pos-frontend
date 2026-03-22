19cb842194b17bf7d4fbf8129894d92faa1b8f87
SRT DEV <sahirruiz4@gmail.com>
2026-03-22 17:37:11 -0500

    feat: Revamp header component with new layout, logo integration, and conditional sidebar button visibility

--- src/app/modules/shared/components/ui/header/header.ts
+++ src/app/modules/shared/components/ui/header/header.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { SidebarStateService } from '../../../services/sidebarState.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-  constructor(public sidebarState: SidebarStateService) {}
+  constructor(
+    public sidebarState: SidebarStateService,
+    public router: Router,
+  ) {}
+
+  hideSidebarButton(): boolean {
+    return this.router.url.includes('/login') || this.router.url.includes('/register');
+  }
 }
