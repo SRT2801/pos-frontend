6b2b0e884a31eeb49664dda03cd9814795c4c527
SRT DEV <sahirruiz4@gmail.com>
2026-03-24 15:21:59 -0500

    feat: Locate registration page and improve input components

--- src/app/modules/pages/register/register.ts
+++ src/app/modules/pages/register/register.ts
@@ -5,9 +5,12 @@ import { ToastService } from '../../services/toast.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { InputComponent } from '../../shared/components/ui/input/input';
+import { PasswordInputComponent } from '../../shared/components/ui/input/password-input';
+import { ProgressSpinnerService } from '../../services/progress-spinner.service';
 
 @Component({
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, InputComponent, PasswordInputComponent],
   selector: 'app-register',
   templateUrl: './register.html',
   styleUrl: './register.css',
@@ -28,7 +31,16 @@ export class RegisterPage {
   constructor(
     private authService: AuthService,
     private toast: ToastService,
-  ) {}
+    private spinner: ProgressSpinnerService,
+  ) {
+    this.email.set('');
+    this.password.set('');
+    this.confirmPassword.set('');
+    this.error.set('');
+    this.success.set('');
+    this.emailError.set('');
+    this.passwordError.set('');
+  }
 
   get passwordsMatch() {
     return this.password() === this.confirmPassword() && this.confirmPassword() !== '';
@@ -80,18 +92,20 @@ export class RegisterPage {
     }
 
     this.error.set('');
-
+    this.spinner.show();
     this.authService.register(formData).subscribe({
       next: () => {
         this.toast.success('Registro exitoso');
         this.email.set('');
         this.password.set('');
         this.confirmPassword.set('');
+        this.spinner.hide();
       },
       error: (err) => {
         const message = err.error?.message || 'Error en el registro';
         this.error.set(message);
         this.toast.error(message);
+        this.spinner.hide();
       },
     });
   }
