6d98362895c2adcd66dd03f84d6f280bc58c94c6
SRT DEV <sahirruiz4@gmail.com>
2026-03-24 15:24:16 -0500

    feat: Integrate AppProgressSpinner and improve the layout structure

--- src/app/modules/shared/layout/layout.ts
+++ src/app/modules/shared/layout/layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Sidebar } from '../components/ui/sidebar/sidebar';
 import { Header } from '../components/ui/header/header';
@@ -6,14 +6,18 @@ import { Router, RouterModule } from '@angular/router';
 import { SidebarStateService } from '../services/sidebarState.service';
 import { ToastModule } from 'primeng/toast';
 import { usesMinimalLayout } from './minimal-layout.util';
+import { AppProgressSpinner } from '../components/ui/progress-spinner/progress-spinner';
+import { ProgressSpinnerService } from '../../services/progress-spinner.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterModule, FormsModule, Sidebar, Header, ToastModule],
+  imports: [RouterModule, FormsModule, Sidebar, Header, ToastModule, AppProgressSpinner, CommonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class Layout {
+  protected readonly spinner = inject(ProgressSpinnerService);
   constructor(
     public sidebarState: SidebarStateService,
     public router: Router,
