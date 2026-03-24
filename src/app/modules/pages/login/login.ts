5854df825160195f77d3f6422412cb746cad53f1
SRT DEV <sahirruiz4@gmail.com>
2026-03-24 15:21:32 -0500

    feat: Localize login page and integrate input components for email and password

--- src/app/modules/pages/login/login.ts
+++ src/app/modules/pages/login/login.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { InputComponent } from '../../shared/components/ui/input/input';
+import { PasswordInputComponent } from '../../shared/components/ui/input/password-input';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, InputComponent, PasswordInputComponent],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class LoginComponent {
+  email = signal('');
+  password = signal('');
+  showPassword = signal(false);
 
+  togglePassword() {
+    this.showPassword.set(!this.showPassword());
+  }
+
+  // Aquí podrías agregar lógica de login si lo deseas
 }
