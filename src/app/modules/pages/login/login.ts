1a050e1db3f580f1daf5fc6db1a154c3c5d2a3c1
SRT DEV <sahirruiz4@gmail.com>
2026-03-24 23:22:20 -0500

    feat: Implement login functionality with validation and error handling

--- src/app/modules/pages/login/login.ts
+++ src/app/modules/pages/login/login.ts
@@ -4,6 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { InputComponent } from '../../shared/components/ui/input/input';
 import { PasswordInputComponent } from '../../shared/components/ui/input/password-input';
+import { AuthService } from '../../services/auth.service';
+
+import { loginSchema } from '../../shared/schemas/login.schema';
+import { ProgressSpinnerService } from '../../services/progress-spinner.service';
+import { ToastService } from '../../services/toast.service';
 
 @Component({
   selector: 'app-login',
@@ -13,12 +18,71 @@ import { PasswordInputComponent } from '../../shared/components/ui/input/passwor
 })
 export class LoginComponent {
   email = signal('');
+  emailError = signal('');
   password = signal('');
   showPassword = signal(false);
+
+  error = signal('');
+  success = signal('');
 
   togglePassword() {
     this.showPassword.set(!this.showPassword());
   }
 
-  // Aquí podrías agregar lógica de login si lo deseas
+  constructor(
+    private authService: AuthService,
+    private spinner: ProgressSpinnerService,
+    private toast: ToastService,
+  ) {
+    this.email.set('');
+    this.password.set('');
+    this.emailError.set('');
+    this.error.set('');
+    this.success.set('');
+  }
+
+  login() {
+    this.error.set('');
+    this.success.set('');
+    this.emailError.set('');
+    const formdata = {
+      email: this.email(),
+      password: this.password(),
+    };
+
+    const result = loginSchema.safeParse(formdata);
+
+    if (!result.success) {
+      this.email.set('');
+      this.password.set('');
+
+      result.error.issues.forEach((issue) => {
+        if (issue.path.includes('email')) {
+          this.emailError.set(issue.message);
+        }
+        if (issue.path.includes('password')) {
+          this.password.set('');
+        }
+      });
+      return;
+    }
+
+    this.error.set('');
+    this.spinner.show();
+    this.authService.login(formdata).subscribe({
+      next: () => {
+        this.toast.success('Login successful');
+        this.email.set('');
+        this.password.set('');
+        this.spinner.hide();
+      },
+      error: (err) => {
+        const message = err.error?.message || 'Login failed';
+        this.error.set(message);
+        this.toast.error(message);
+        this.password.set('');
+        this.spinner.hide();
+      },
+    });
+  }
 }
