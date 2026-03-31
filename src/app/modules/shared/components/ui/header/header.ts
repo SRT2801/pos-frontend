f8733b4811014cd41e1b87eee30263720b3b61a3
SRT DEV <sahirruiz4@gmail.com>
2026-03-31 00:57:12 -0500

    refactor: Remove SidebarStateService and related methods from Header component

--- src/app/modules/shared/components/ui/header/header.ts
+++ src/app/modules/shared/components/ui/header/header.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SidebarStateService } from '../../../services/sidebarState.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { usesMinimalLayout } from '../../../layout/minimal-layout.util';
@@ -11,12 +10,7 @@ import { usesMinimalLayout } from '../../../layout/minimal-layout.util';
   styleUrl: './header.css',
 })
 export class Header {
-  constructor(
-    public sidebarState: SidebarStateService,
-    public router: Router,
-  ) {}
+  
 
-  hideSidebarButton(): boolean {
-    return usesMinimalLayout(this.router.url);
-  }
+
 }
